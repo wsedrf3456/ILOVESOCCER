dbcc4a0c73b4ae6f013fa520b972deae45e14a8f
김성빈 <wsedrf3456@naver.com>
2017-10-23 14:15:49 +0900

    정규식 사용하여 title 태그 따오기

--- crawling.py
+++ crawling.py
@@ -1,6 +1,12 @@
 from urllib.request import urlopen
+import re
 
+# 정규식으로 title 부분 따오기
+p = re.compile('<title>.*</title>')
 url = 'http://sports.news.naver.com/wfootball/news/index.nhn'
 f = urlopen(url)
-b = f.read()
-print(b)
+buf = f.read()
+buf2 = str(buf)
+m = p.search(buf2)
+print(m)
+
