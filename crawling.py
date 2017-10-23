b7650f98e08c190fb3ab1b428315af22731ce29d
김성빈 <wsedrf3456@naver.com>
2017-10-23 14:30:13 +0900

    그룹화해서 title 안에 내용만 가져오기!

--- crawling.py
+++ crawling.py
@@ -2,11 +2,11 @@ from urllib.request import urlopen
 import re
 
 # 정규식으로 title 부분 따오기
-p = re.compile('<title>.*</title>')
+p = re.compile('<title>(.*)</title>')
 url = 'http://sports.news.naver.com/wfootball/news/index.nhn'
 f = urlopen(url)
 buf = f.read()
 buf2 = str(buf)
 m = p.search(buf2)
-print(m)
+print(m.group(1))
 
