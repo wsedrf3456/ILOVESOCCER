da66d2a6f019c30777985bf850825bb414e57ab8
김성빈 <wsedrf3456@naver.com>
2017-10-24 17:08:16 +0900

    기사제목따오기git add crawling.py

--- crawling.py
+++ crawling.py
@@ -1,12 +1,23 @@
 from urllib.request import urlopen
 import re
+import json
 
 # 정규식으로 title 부분 따오기
-p = re.compile('<title>(.*)</title>')
+p = re.compile('(newsListModel)(.*)')
 url = 'http://sports.news.naver.com/wfootball/news/index.nhn'
 f = urlopen(url)
-buf = f.read()
-buf2 = str(buf)
-m = p.search(buf2)
-print(m.group(1))
+buf = f.read().decode('utf-8')
+m = p.search(buf)
 
+# json 모듈이 이용하여 사전 객체로 변환하기
+data = m.group(2)[2:]
+data2 = json.loads(data)
+data3 = data2['list']
+
+# key 가 title 인 것의 value 값 따오기
+n = 1
+for i in data3 :
+	for a,b in i.items():
+		if a == 'title':
+			print(('%d 번째 기사 : ' % n), b)
+			n += 1
