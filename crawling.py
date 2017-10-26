1f15f5c28cfed64800a1d370608acd9c2e364d6b
김성빈 <wsedrf3456@naver.com>
2017-10-27 00:24:21 +0900

    링크추가git add crawling.py

--- crawling.py
+++ crawling.py
@@ -2,22 +2,37 @@ from urllib.request import urlopen
 import re
 import json
 
-# 정규식으로 title 부분 따오기
-p = re.compile('(newsListModel)(.*)')
+# 네이버 스포츠 해외 축구 최신뉴스
 url = 'http://sports.news.naver.com/wfootball/news/index.nhn'
+
+# http 응답 객체 생성
+# 웹 페이지 전체 소스 코드 가져오기
 f = urlopen(url)
 buf = f.read().decode('utf-8')
+f.close()
+
+# 원하는 데이터가 들어 있는 json 객체를 정규식을 이용해서 파싱! 
+p = re.compile('(newsListModel)(.*)')
 m = p.search(buf)
+data = m.group(2)[2:]
 
 # json 모듈이 이용하여 사전 객체로 변환하기
-data = m.group(2)[2:]
 data2 = json.loads(data)
 data3 = data2['list']
 
-# key 가 title 인 것의 value 값 따오기
+# link 만들기!
+link_base = "http://sports.news.naver.com/wfootball/news/read.nhn?oid=%s&aid=%s"
+
+# key 가 title, aid, oid 인 것의 value 값 파싱!
 n = 1
 for i in data3 :
+	# 원하는 값인 oid 와 aid 를 따로 변수에 담는다.
+	oid = i['oid']
+	aid = i['aid']
+	link = link_base % (oid, aid)
+	print(('%d 번째 링크 : ' %n), link)
+
 	for a,b in i.items():
 		if a == 'title':
-			print(('%d 번째 기사 : ' % n), b)
-			n += 1
+			print(('%d 번째 기사 : ' %n), b)
+			n+=1
