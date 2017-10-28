f05344c9c520d48ee6da5df3ff7c1f725230b7db
김성빈 <wsedrf3456@naver.com>
2017-10-28 14:08:24 +0900

    중복 제거 및 예외 처리 추가

--- soccerdata.py
+++ soccerdata.py
@@ -1,13 +1,24 @@
 import sqlite3
 import crawling
 
-con = sqlite3.connect('soccer.db')
-cur = con.cursor()
+try:
+	# DB 연결, SQL 구문 호출 위해 객체 생성
+	con = sqlite3.connect('soccer.db')
+	cur = con.cursor()
 
-sql = 'insert into soccer values (?,?);'
+	sql = 'insert into soccer values (?,?,?);'
 
-records = crawling.getNews()
-for record in records :
-	cur.execute(sql,record)
-con.commit()
+	# crawling 파일의 getNews 함수 호출
+	records = crawling.getNews()
+
+	# 웹 페이지 data 들을 sql 형태에 맞게 넣기
+	for record in records :
+		cur.execute(sql,record)
+
+	# 작업한 내용을 실제로 DB에 반영
+	con.commit()
+except:
+	pass
+
+# DB 연결 닫기
 con.close()
