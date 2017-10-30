b90454415adbd8c7dd763d6286423023fe4c3d48
김성빈 <wsedrf3456@naver.com>
2017-10-31 00:46:40 +0900

    오류처리 수정

--- soccerdata.py
+++ soccerdata.py
@@ -1,24 +1,24 @@
 import sqlite3
 import crawling
 
-try:
-	# DB 연결, SQL 구문 호출 위해 객체 생성
-	con = sqlite3.connect('soccer.db')
-	cur = con.cursor()
+# DB 연결, SQL 구문 호출 위해 객체 생성
+con = sqlite3.connect('soccer.db')
+cur = con.cursor()
 
-	sql = 'insert into soccer values (?,?,?);'
+sql = 'insert into soccer values (?,?,?);'
 
-	# crawling 파일의 getNews 함수 호출
-	records = crawling.getNews()
+# crawling 파일의 getNews 함수 호출
+records = crawling.getNews()
 
+try :
 	# 웹 페이지 data 들을 sql 형태에 맞게 넣기
 	for record in records :
 		cur.execute(sql,record)
-
-	# 작업한 내용을 실제로 DB에 반영
-	con.commit()
-except:
+except :
 	pass
+
+# 작업한 내용을 실제로 DB에 반영
+con.commit()
 
 # DB 연결 닫기
 con.close()
