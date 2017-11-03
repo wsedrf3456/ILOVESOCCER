91e833c6fd2b38b9312a029c4c9bdbc1fd9c8372
김성빈 <wsedrf3456@naver.com>
2017-11-03 16:15:26 +0900

    함수화

--- soccerdata.py
+++ soccerdata.py
@@ -1,24 +1,26 @@
 import sqlite3
 import crawling
 
-# DB 연결, SQL 구문 호출 위해 객체 생성
-con = sqlite3.connect('soccer.db')
-cur = con.cursor()
+def putData():
+	# DB 연결, SQL 구문 호출 위해 객체 생성
+	con = sqlite3.connect('soccer.db')
+	cur = con.cursor()
 
-sql = 'insert into soccer values (?,?,?);'
+	sql = 'insert into soccer values (?,?,?);'
 
-# crawling 파일의 getNews 함수 호출
-records = crawling.getNews()
+	# crawling 파일의 getNews 함수 호출
+	records = crawling.getNews()
 
-try :
-	# 웹 페이지 data 들을 sql 형태에 맞게 넣기
-	for record in records :
-		cur.execute(sql,record)
-except :
-	pass
+	try :
+		# 웹 페이지 data 들을 sql 형태에 맞게 넣기
+		for record in records :
+			cur.execute(sql,record)
+	except :
+		pass
 
-# 작업한 내용을 실제로 DB에 반영
-con.commit()
+	# 작업한 내용을 실제로 DB에 반영
+	con.commit()
 
-# DB 연결 닫기
-con.close()
+	# DB 연결 닫기
+	con.close()
+	return records
